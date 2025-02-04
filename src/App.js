1873b9a1ba632d0b69bd43bfa809e0ad4f83b096
Ahsan-Zaidi98 <ahsanzaidi068@gmail.com>
2025-02-04 18:22:53 +0500

    update routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";  // changed to HashRouter
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Home from "./components";
 import Resume from "./components/Resume";
@@ -14,12 +14,14 @@ function App() {
     <React.Fragment>
       <CssBaseline />
       <Navbar />
-      <Switch>
-        <Route exact path="Home/" component={Home} />
-        <Route exact path="/resume" component={Resume} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
-      </Switch>
+      <Router>  {/* Changed BrowserRouter to HashRouter */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/resume" component={Resume} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </Router>
     </React.Fragment>
   );
 }
