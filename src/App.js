7051755da8ad5b17e7dfceb066a27fbd65bb7ab0
Ahsan-Zaidi98 <ahsanzaidi068@gmail.com>
2025-02-04 18:20:04 +0500

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <CssBaseline />
       <Navbar />
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="Home/" component={Home} />
         <Route exact path="/resume" component={Resume} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact} />
